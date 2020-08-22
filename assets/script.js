60261e8c044a85aa9d6f02c8dba1299a7f425673
Anastasia Volosevych <vestyankas@gmail.com>
2020-08-22 13:17:00 -0600

    fix

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,21 @@
-var input = document.querySelector('#user-input');
+// timeblock inputs
+var nine = document.querySelector('#user-input9');
+var ten = document.querySelector('#user-input10');
+var eleven = document.querySelector('#user-input11');
+var twelve = document.querySelector('#user-input12');
+var thirteen = document.querySelector('#user-input13');
+var fourteen = document.querySelector('#user-input14');
+var fifteen = document.querySelector('#user-input15');
+var sixteen = document.querySelector('#user-input16');
+var seventeen = document.querySelector('#user-input17');
 
-var saveBtn = document.querySelector("#save");
+// save button
+var saveBtn = document.querySelector("#save")
+
+// variable for current day
 var today = document.querySelector("#currentDay");
 
-// current day
+// displaying current day
 var momentElement = moment().hours(Number);
 today.innerHTML = (momentElement);
 
