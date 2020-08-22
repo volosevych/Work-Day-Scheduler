9aad4397c95bc37c90dac523a08fd227c0ca2951
Anastasia Volosevych <vestyankas@gmail.com>
2020-08-22 15:35:00 -0600

    finished code with local storage and past/present/future blocks

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,59 @@
-// timeblock inputs
-var nine = document.querySelector('#user-input9');
-var ten = document.querySelector('#user-input10');
-var eleven = document.querySelector('#user-input11');
-var twelve = document.querySelector('#user-input12');
-var thirteen = document.querySelector('#user-input13');
-var fourteen = document.querySelector('#user-input14');
-var fifteen = document.querySelector('#user-input15');
-var sixteen = document.querySelector('#user-input16');
-var seventeen = document.querySelector('#user-input17');
+// essentially tells engine to load 1)html & 2)css first.
+$(document).ready(function () {
 
-// save button
-var saveBtn = document.querySelector("#save")
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-// variable for current day
-var today = document.querySelector("#currentDay");
+    //add event listener to our button
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
 
-// displaying current day
-var momentElement = moment().hours(Number);
-today.innerHTML = (momentElement);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+    
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+    function hourTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour, currentHour)
+
+            //check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
+})
